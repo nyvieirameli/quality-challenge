ee1ad8eacd0cf1cfacb9f6a07b190d7d1235af14
Nycolas De Lima Vieira <nyvieira@mbpdemeliadmin.lan>
2021-06-10 14:41:10 -0300

    project first commit

---
+++ src/main/java/br/com/digitalhouse/bootcamp/qualitychallenge/QualitychallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.digitalhouse.bootcamp.qualitychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualitychallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualitychallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/digitalhouse/bootcamp/qualitychallenge/QualitychallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digitalhouse.bootcamp.qualitychallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualitychallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
